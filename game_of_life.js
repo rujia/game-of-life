2f75bbbd2072bac62d2b6c73635a3a9cdf471473
Rujia Zha <rujiazha@gmail.com>
2014-09-17 20:01:23 -0400

    added rep invariant

--- game_of_life.js
+++ game_of_life.js
@@ -2,9 +2,13 @@ var GameOfLife = function() {
 	var self = {};
 
 	//lifeRep is the representation of the board of cells
+	//rep invariant: upon calling any function, lifeRep is a MAX_Y by MAX_X array, 
+	//and all values are either 1 or 0. 
 	var lifeRep = []; 
 	//each cell in neighbors has a value equal to the number of 
 	//neighbors with a value of 1 of the corresponding cell in lifeRep.
+	//rep invariant: upon calling any function, neighbors is a MAX_Y+2 by MAX_X+2 
+	//array, and all values are greater than or equal to 0
 	var neighbors = []; 
 
 	self.board = lifeRep;
@@ -12,10 +16,36 @@ var GameOfLife = function() {
 	var MAX_X = 10;
 	var MAX_Y = 10;
 
+	//Makes sure that neighbors is at least a MAX_Y+2 by MAX_X+2 
+	//array, and all values are greater than or equal to 0
+	neighbors.checkRep = function(){
+		for (var j=-1; j<MAX_Y+1; j++){
+			for (var i = -1; i<MAX_X+1; i++){
+				if (typeof this[j][i] != "number" && this[j][i] <0){
+					throw {name: 'neighborRepInvariantException', message: "Rep invariant was broken"};
+				}
+			}
+		}
+	}
+
+	//LifeRep is a MAX_Y by MAX_X array, 
+	//and all values are either 1 or 0. 
+	lifeRep.checkRep = function() {
+		if (!(this.length == MAX_Y && this[0].length == MAX_X)){
+			throw {name: "lifeRepInvariantException", message: "Rep invariant was broken"};
+		}
+		for (var j=0; j<MAX_Y; j++){
+			for (var i = 0; i<MAX_X; i++){
+				if (!(this[j][i]==0 || this[j][i]==1)){
+					throw {name: "lifeRepInvariantException", message: "Rep invariant was broken"};
+				}
+			}
+		}
+	}
+
 	//Takes in the position of the cell in lifeRep, 
 	//where x is the column index and y is the row index.
 	//it then adds one to the surrounding cells in neighbors.
-	//size of neighbors should stay the same.
 	neighbors.increment = function(x, y){
 		for (var i = x-1; i<=x+1; i++){
 			for (var j = y-1; j<=y+1; j++){
@@ -23,10 +53,10 @@ var GameOfLife = function() {
 			}
 		}
 		this[y][x]-= 1;
+		this.checkRep();
 	}
 
 	//Updates neighbors to accomodate for updated lifeRep.
-	//size of neighbors should remain the same.
 	neighbors.update = function(){
 		this.reset();
 		for (var j=0; j<MAX_Y; j++){
@@ -36,6 +66,7 @@ var GameOfLife = function() {
 				}
 			}
 		}
+		this.checkRep();
 	}
 
 	//Resets neighbors to contain all 0's and have the dimensions of
@@ -48,6 +79,7 @@ var GameOfLife = function() {
 			}
 			this[j]= neighborRow;
 		}
+		this.checkRep();
 	}
 
 	//Source equals a double array of 1's and 0's with dimensions MAX_Y by MAX_X. 
@@ -70,6 +102,7 @@ var GameOfLife = function() {
 			}
 		}
 		neighbors.reset();
+		lifeRep.checkRep();
 	}
 
 	//Updates neighbors, then update lifeRep based on the value of each cell and
@@ -80,7 +113,6 @@ var GameOfLife = function() {
 	//then the cell is updated to 0.
 	//if a cell has a value of 0 and has exactly 3 neighbors with a value of 1, 
 	//then the cell is updated to 1. 
-	//lifeRep should remain the same size. 
 	self.update = function(){
 		neighbors.update();
 		for (var j=0; j<MAX_Y; j++){
@@ -97,6 +129,9 @@ var GameOfLife = function() {
 				}
 			}
 		}
+		lifeRep.checkRep();
+		neighbors.checkRep();
+		//console.log(neighbors);
 	}
 	return self;
 }
